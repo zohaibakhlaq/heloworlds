4b10c47393a1f6bbb95d41de03507a4c808175c3
zaheer <zaheer@gmail.com>
2021-08-10 17:40:28 +0500

    Executed first time

---
+++ my website/Controllers/detailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using my_website.Models;
+
+namespace my_website.Controllers
+{
+    public class detailsController : Controller
+    {
+        private mydbEntities db = new mydbEntities();
+
+        // GET: details
+        public ActionResult Index()
+        {
+            return View(db.details.ToList());
+        }
+
+        // GET: details/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detail detail = db.details.Find(id);
+            if (detail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detail);
+        }
+
+        // GET: details/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: details/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,email,password")] detail detail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.details.Add(detail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(detail);
+        }
+
+        // GET: details/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detail detail = db.details.Find(id);
+            if (detail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detail);
+        }
+
+        // POST: details/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,name,email,password")] detail detail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(detail);
+        }
+
+        // GET: details/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detail detail = db.details.Find(id);
+            if (detail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detail);
+        }
+
+        // POST: details/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            detail detail = db.details.Find(id);
+            db.details.Remove(detail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
